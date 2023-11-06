79b9f9f69a831ea661b7b137c8b1300eb4ba1dda
prajapatisweta <s.p.2410.s.p@gmail.com>
2023-11-06 16:15:04 +0530

    Smooth navigation

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -32,8 +33,6 @@ document.addEventListener('keydown', function (e) {
 });
 
 // Smooth scrolling effect
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
 
 btnScrollTo.addEventListener('click', function(e) {
   const s1coords = section1.getBoundingClientRect();
@@ -53,6 +52,32 @@ btnScrollTo.addEventListener('click', function(e) {
   section1.scrollIntoView({behavior: 'smooth'});
 });
 
+/////// Event listeners
+// Page navigation
+
+// --------- Scroll but not optimal ---------
+// document.querySelectorAll('.nav__link').forEach(function(el) {
+//   el.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+//   });
+// });
+
+// --------- For better optimal scroll event ---------
+// Using Event Delegation
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function(e) {
+  e.preventDefault();
+
+  // Matching if the element is clicked
+  if(e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+  }
+})
 
 ///////////////////////////////////////////////////////
 
