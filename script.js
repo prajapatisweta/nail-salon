71cec3d4aa1d07eb1c61525e7eab4b6132b3149f
prajapatisweta <s.p.2410.s.p@gmail.com>
2023-11-09 18:58:30 +0530

    Scroll reveal of sections and lazy loading of images

--- script.js
+++ script.js
@@ -38,22 +38,22 @@ document.addEventListener('keydown', function (e) {
 
 // Smooth scrolling effect
 
-btnScrollTo.addEventListener('click', function(e) {
+btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
   console.log(s1coords);
 
   console.log(e.target.getBoundingClientRect());
 
   // Scrolling
-/////////Old Way////////////
+  /////////Old Way////////////
   // window.scrollTo({
   //   left: s1coords.left + window.pageXOffset,
   //   top: s1coords.top + window.pageYOffset,
   //   behavior: 'smooth'
   // });
 
-// Modern Way ---------- just one line needed
-  section1.scrollIntoView({behavior: 'smooth'});
+  // Modern Way ---------- just one line needed
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
 
 /////// Event listeners
@@ -73,53 +73,55 @@ btnScrollTo.addEventListener('click', function(e) {
 // 1. Add event listener to common parent element
 // 2. Determine what element originated the event
 
-document.querySelector('.nav__links').addEventListener('click', function(e) {
+document.querySelector('.nav__links').addEventListener('click', function (e) {
   e.preventDefault();
 
   // Matching if the element is clicked
-  if(e.target.classList.contains('nav__link')) {
+  if (e.target.classList.contains('nav__link')) {
     const id = e.target.getAttribute('href');
-    document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
-})
+});
 
 // Tabbed component
 
 // *** this method is not optimal as it will create copy
-// tabs.forEach(t => t.addEventListener('click', () => 
+// tabs.forEach(t => t.addEventListener('click', () =>
 // console.log('tab')));
 
 // *** using event delegation for optimal usage
-tabsContainer.addEventListener('click', function(e) {
+tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
 
   // ignoring any other click in that area
-  if(!clicked) return;
+  if (!clicked) return;
 
   // remove active class
   tabs.forEach(t => t.classList.remove('operations__tab--active'));
   tabsContent.forEach(c => c.classList.remove('operations__content--active'));
-  
+
   // activate tab
   clicked.classList.add('operations__tab--active');
 
   // Activate Content area
-  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
 
 // Menu Fade animation
-const handleHover = function(e){
-  if(e.target.classList.contains('nav__link')){
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
     const logo = link.closest('.nav').querySelector('img');
 
     siblings.forEach(el => {
-      if(el !== link) el.style.opacity = this;
+      if (el !== link) el.style.opacity = this;
     });
     logo.style.opacity = this;
   }
-}
+};
 
 // passing arguments into handler
 nav.addEventListener('mouseover', handleHover.bind(0.5));
@@ -127,18 +129,87 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
 // Sticky Navigation
-const initialCoords = section1.getBoundingClientRect();
+/* const initialCoords = section1.getBoundingClientRect();
 console.log(initialCoords);
 
 window.addEventListener('scroll', function() {
   console.log(window.scrollY);
   if(window.scrollY > initialCoords.top) nav.classList.add('sticky');
   else nav.classList.remove('sticky')
-});
-///////////////////////////////////////////////////////
-///////////////////////////////////////////////////////
+}); */
 
-const header = document.querySelector('.header');;
+// Sticky Navigation using intersection observer api
+
+const header = document.querySelector('.header');
+const navheight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navheight}px`,
+});
+headerObserver.observe(header);
+
+// Reveal section on scroll using IntersectionObserver API
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  // to make it visible on the 1st section as well because it was alreday returning the 1st entry
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+// Lazy Loading Images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loading = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // replace the src of image with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loading, {
+  root: null,
+  threshold: 0,
+  rootMargin: '-20px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
+// Slider component
+
+
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
 
 const message = document.createElement('div');
 message.classList.add('cookie-message');
@@ -149,21 +220,21 @@ message.innerHTML = `We use cookies for improved functionality and analytics <bu
 // header.after(message);
 
 // removing cookie
-document.querySelector('.btn--close-cookie').addEventListener('click', function() {
-  message.remove();
+// document.querySelector('.btn--close-cookie').addEventListener('click', function() {
+//   message.remove();
 
-  // Old method of removing
-  // message.parentElement.removeChild(message);
-});
+//   // Old method of removing
+//   // message.parentElement.removeChild(message);
+// });
 
 //styles
-message.style.backgroundColor = '#37383d'
+message.style.backgroundColor = '#37383d';
 
 // *****************************************
 const h1 = document.querySelector('h1');
 
 // --------- Going downwards
-// h1.querySelectorAll('.highlights');
+// h1.querySelectorAll('.highlight');
 // h1.childNodes;
 // h1.children;
 h1.firstElementChild.style.color = 'white';
